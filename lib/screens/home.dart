363e456cb25cab168682a0c623f36cab1fa855f2
AyushShukla3 <ayushshukla30339@gmail.com>
2021-12-27 15:13:23 -0800

    Working bottom navigation bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'browse.dart';
+import 'host.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  static List<Widget> _widgetOptions = <Widget>[Browse(), Host()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('welcome to raffle app'),
-      ),
-    );
+        appBar: AppBar(title: Text('Welcome to raffle app')),
+        body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Browse'),
+            BottomNavigationBarItem(icon: Icon(Icons.poll), label: 'Host')
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.blue,
+          onTap: _onItemTapped,
+        ));
   }
 }
